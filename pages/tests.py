a38b838ba9f6e938567da7d8d9f173411ebd891d
Girish K01 <girishkcoding01@gmail.com>
2022-07-18 19:42:00 +0530

    New commit using commit and push button in vscode

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
 
 # Create your tests here.
+class Simpletests(SimpleTestCase):
+    def testhp(self):
+        response=self.client.get('/')
+        self.assertEqual(response.status_code,200)
+    def testap(self):
+        response=self.client.get('/about/')
+        self.assertEqual(response.status_code,200)    
